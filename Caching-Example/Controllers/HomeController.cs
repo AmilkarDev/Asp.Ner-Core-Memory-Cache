6a0fcf8d999b7835c168c8fcd6f3f20f2e5ede72
amilkardev <melek.ferhi@gmail.com>
2019-04-12 00:33:47 +0100

    Add project files.

---
+++ Caching-Example/Controllers/HomeController.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Caching_Example.Models;
+using Microsoft.Extensions.Caching.Memory;
+using Caching;
+using Caching.Models;
+
+namespace Caching.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IMemoryCache _memoryCache;
+        public class CacheValueAndsource
+        {
+            public string EmployeeName { get; set; }
+            public string Source { get; set; }
+        }
+
+
+        public CacheValueAndsource CacheFirstWay()
+        {
+            string cacheValue;
+            string sourceOfData = "Cache";
+            CacheValueAndsource cvs = new CacheValueAndsource();
+            cvs.EmployeeName = null;
+            cvs.Source = null;
+            // Look for cache key.
+            if (!_memoryCache.TryGetValue<string>("_EmployeeName", out cacheValue))
+            {
+                // Key not in cache, so get data.
+                EmployeeViewModel model = new EmployeeViewModel(1,2,"engineer", "malek");
+                cacheValue = model.emp.fullName;
+                sourceOfData = "Not Cache";
+
+                //Cache expiration
+                var cacheEntryOptions = new MemoryCacheEntryOptions()
+                    .SetSlidingExpiration(TimeSpan.FromMinutes(1));
+
+                // Save values in cache for a given key.
+                _memoryCache.Set<string>("_EmployeeName", cacheValue, cacheEntryOptions);
+            }
+            cvs.EmployeeName = cacheValue.ToString();
+            cvs.Source = sourceOfData.ToString();
+            return cvs;
+        }
+
+        public string EmployeeDetails()
+        {
+            var value = _memoryCache.Get<string>("_EmployeeName");
+
+            string cacheValue;
+            if (!_memoryCache.TryGetValue<string>("_EmployeeName", out cacheValue))
+            {
+                value= "the cache is empty , so the app will fill it with the name Jhon , reload the page to see that !";
+            }
+
+            _memoryCache.GetOrCreate<string>("_EmployeeName",
+                  cacheEntry => {
+                      return "John";
+                  });
+            return value;
+        }
+
+
+        public HomeController(IMemoryCache memoryCache)
+        {
+            this._memoryCache = memoryCache;
+        }
+        public IActionResult Index()
+        {
+            CacheValueAndsource vv = CacheFirstWay();
+            ViewData["name"] = vv.EmployeeName;
+            ViewData["source"] = vv.Source;
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = EmployeeDetails();
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Caching-Example/Models/EmployeeViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Caching.Models
+{
+            public class EmployeeViewModel
+            {
+            public EmployeeViewModel(int id1,int id2,string jobb , string name)
+            {
+            emp = new Employee() { Id = id2, fullName = name };
+                emp.Id = id2;
+                job = jobb;
+            }
+            public int Id { get; set; }
+            public Employee emp { get; set; }
+            public string  job { get; set; }
+            }
+            public class Employee
+            {
+                public int Id { get; set; }
+                public string fullName { get; set; }
+            }
+
+}
